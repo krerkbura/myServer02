129fbddb6f4cb73af3fca69d728b39af96f85b49
Krerkbura <krerkbura@hotmail.com>
2014-11-04 00:46:27 +0700

    add new folder in public and edit index.js

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/book',function(req,res){
+	var books = [
+		{title: 'Ragnarok', price: 200},
+		{title: 'Maple', price: 300},
+		{title: 'Yulgang', price: 400}
+	];
+	res.send(books);
+})
+
 var server = app.listen(3000, function () {
 
   	console.log("server is running")
