ab018d6fdc9b4077eda1a008d17568f65810fbb9
Krerkbura <krerkbura@hotmail.com>
2014-11-12 20:50:59 +0700

    use mongodb

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var express = require('express')
 var app = express()
+var mangojs = requre('mongojs')
+var db = mongojs('my_server',['book']);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/book',function(req,res){
-	var books = [
-		{title: 'Ragnarok', price: 200},
-		{title: 'Maple', price: 300},
-		{title: 'Yulgang', price: 400}
-	];
-	res.send(books);
+	db.book.find({},function(err,books){
+			res.send(books);
+		})
 })
 
 var server = app.listen(3000, function () {
