8cd9cfd06ceb93671646842afe6af262af15e460
Krerkbura <krerkbura@hotmail.com>
2014-11-12 20:57:53 +0700

    insert book in db

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var bodyParser = require('body-parser')
 var app = express()
-var mangojs = requre('mongojs')
 var db = mongojs('my_server',['book']);
 
 app.use(express.static(__dirname + '/public'));
-
-app.get('/api/book',function(req,res){
-	db.book.find({},function(err,books){
-			res.send(books);
-		})
+app.use(bodyParser.json())
+app.get('/api/book', function(req, res){
+	db.book.find({}, function(err,docs){
+		res.send(docs);
+	});
 })
 
-var server = app.listen(3000, function () {
-
-  	console.log("server is running")
+app.post('/api/book', function(req,res){
+	db.book.insert(req.body,function(err, docs){
+		res.send(docs);
+	});
+});
+var server = app.listen(3000, function(){
+	console.log("server is running")
 
 })
